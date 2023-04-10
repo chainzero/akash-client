5fd87590874445106140197260e51011dcb0ac12
Scott Carruthers <scarruthers@Scotts-MacBook-Pro.local>
2023-04-10 15:49:34 -0400

    Transaction message updates

--- akashrpcclient_withtx/main.go
+++ akashrpcclient_withtx/main.go
@@ -22,21 +22,19 @@ func main() {
 	// Start of Msg Create logic
 	// Logic derived from code initiated via: https://github.com/akash-network/node/blob/52d5ee5caa2c6e5a5e59893d903d22fe450d6045/x/deployment/client/cli/tx.go#L83
 
+	// Replace the directory specified with the location of SDL associated with the current deployment
 	sdlLocation := ("./testsdl/deploy.yml")
 	accountPrefix := "akash"
-	bech32Address := "akash1w3k6qpr4uz44py4z68chfrl7ltpxwtkngnc6xk"
 
 	sdlManifest, err := sdl.ReadFile(sdlLocation)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	groups, err := sdlManifest.DeploymentGroups()
-	if err != nil {
-		fmt.Println("Error: ", err)
-	}
-
 	id := v1beta2.DeploymentID{}
+
+	// Replace the Akash address below with the address that should be used for deployment creation
+	bech32Address := "akash1w3k6qpr4uz44py4z68chfrl7ltpxwtkngnc6xk"
 
 	ownerBytes, err := sdk.GetFromBech32(bech32Address, accountPrefix)
 	if err != nil {
@@ -46,8 +44,6 @@ func main() {
 	accOwnerBytes := sdk.AccAddress(ownerBytes)
 
 	id.Owner = utils.String(accOwnerBytes)
-
-	fmt.Println("ID owner: ", id.Owner)
 
 	id.DSeq, err = strconv.ParseUint(utils.BlockHeight(), 10, 64)
 	if err != nil {
@@ -65,7 +61,10 @@ func main() {
 		fmt.Println("Error ParseCoinNormalized: ", err)
 	}
 
-	//depositorAcc := "akash1f53fp8kk470f7k26yr5gztd9npzpczqv4ufud7"
+	groups, err := sdlManifest.DeploymentGroups()
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
 
 	msg := &v1beta2.MsgCreateDeployment{
 		ID:      id,
@@ -86,7 +85,7 @@ func main() {
 
 	///////TX OPERATIONS/////////
 
-	// Account `mywallet` was available in local OS keychain from test machine
+	// Account `chainzero` was available in local OS keychain from test machine
 	accountName := "chainzero"
 	// accountAddress := "akash1w3k6qpr4uz44py4z68chfrl7ltpxwtkngnc6xk"
 
@@ -122,42 +121,7 @@ func main() {
 	}
 
 	// Print response from broadcasting a transaction
-	fmt.Print("MsgCreatePost:\n\n")
+	fmt.Print("Transaction broadcast result:\n\n")
 	fmt.Println(txResp)
-
-	//////QUERY OPERATIONS//////////
-
-	// // Instantiate a query client
-	// queryClient := types.NewQueryClient(client.Context())
-
-	// // Query the blockchain using the client's `Deployments` method
-	// queryResp, err := queryClient.Deployments(ctx, &types.QueryDeploymentsRequest{})
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// type DeploymentID struct {
-	// 	Owner string `protobuf:"bytes,1,opt,name=owner,proto3" json:"owner" yaml:"owner"`
-	// 	DSeq  uint64 `protobuf:"varint,2,opt,name=dseq,proto3" json:"dseq" yaml:"dseq"`
-	// }
-
-	// 	deploymentid := types.DeploymentID{
-	// 		Owner: "akash1f53fp8kk470f7k26yr5gztd9npzpczqv4ufud7",
-	// 		DSeq:  10219997,
-	// 	}
-
-	// 	querydeploymentrequest := types.QueryDeploymentRequest{
-	// 		ID: deploymentid,
-	// 	}
-
-	// 	// Query the blockchain using the client's `Deployment` method
-	// 	queryResp, err := queryClient.Deployment(ctx, &querydeploymentrequest)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-
-	// 	// Print response from querying all the posts
-	// 	fmt.Print("\n\nAll posts:\n\n")
-	// 	fmt.Println(queryResp)
 
 }
